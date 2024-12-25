917f5c7a6da07f1dc64295bb8fa5d9dae62c43b0
nitya-kulkarni <nityakulkarni11@gmail.com>
2024-12-25 23:09:07 +0530

    deleted

--- src/main/java/com/example/swarainfra/repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.swarainfra.repository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.swarainfra.model.Contact;
-
-public interface ContactRepository extends MongoRepository<Contact, String> {
-}
